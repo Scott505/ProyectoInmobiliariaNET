39b36d5f99bbacf76a852aa1bcd88dd79a94c844
Santy Sepulveda <santysepu@hotmail.com>
2025-09-05 00:00:58 -0300

    Primer commit - proyecto inmobiliaria NET con ABM de Propietarios e Inquilinos

---
+++ Controllers/InquilinosController.cs
@@ -0,0 +1,86 @@
+using Microsoft.AspNetCore.Mvc;
+using _Net.Models;
+
+namespace _Net.Controllers;
+
+public class InquilinosController : Controller
+{
+    private readonly InquilinosRepository repository;
+    private readonly IConfiguration config;
+
+    public InquilinosController(IConfiguration config)
+    {
+        this.repository = new InquilinosRepository(config);
+        this.config = config;
+    }
+
+    public ActionResult Index()
+    {
+        var lista = repository.ObtenerTodos();
+        return View(lista);
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(Inquilino inquilino)
+    {
+        if (ModelState.IsValid)
+        {
+            repository.Alta(inquilino);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(inquilino);
+    }
+
+    public ActionResult Eliminar(int id)
+    {
+        var Inquilino = repository.ObtenerPorId(id);
+        if (Inquilino == null)
+        {
+            return NotFound();
+        }
+        return View("Delete", Inquilino);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult ConfirmarEliminacion(int IdInquilino)
+    {
+        repository.Baja(IdInquilino);
+        TempData["Mensaje"] = "Eliminación realizada correctamente";
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public ActionResult Edit(int id)
+    {
+        var Inquilino = repository.ObtenerPorId(id);
+        if (Inquilino == null)
+        {
+            return NotFound(); 
+        }
+        return View(Inquilino); 
+    }
+
+    // POST: Inquilinos/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(Inquilino inquilino)
+    {
+        if (ModelState.IsValid)
+        {
+            repository.Modificar(inquilino);
+            TempData["Mensaje"] = "Inquilino actualizado correctamente";
+            return RedirectToAction(nameof(Index));
+        }
+        return View(inquilino);
+    }
+
+
+
+}
---
+++ Controllers/PropietariosController.cs
@@ -0,0 +1,87 @@
+using Microsoft.AspNetCore.Mvc;
+using _Net.Models;
+
+namespace _Net.Controllers;
+
+public class PropietariosController : Controller
+{
+    private readonly PropietariosRepository repository;
+    private readonly IConfiguration config;
+
+    public PropietariosController(IConfiguration config)
+    {
+        this.repository = new PropietariosRepository(config);
+        this.config = config;
+    }
+
+    public ActionResult Index()
+    {
+        var lista = repository.ObtenerTodos();
+        return View(lista);
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(Propietario propietario)
+    {
+        if (ModelState.IsValid)
+        {
+            repository.Alta(propietario);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(propietario);
+    }
+
+    public ActionResult Eliminar(int id)
+    {
+        var propietario = repository.ObtenerPorId(id);
+        if (propietario == null)
+        {
+            return NotFound();
+        }
+        return View("Delete", propietario);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult ConfirmarEliminacion(int IdPropietario)
+    {
+        repository.Baja(IdPropietario);
+        TempData["Mensaje"] = "Eliminación realizada correctamente";
+
+        return RedirectToAction(nameof(Index));
+    }
+
+
+    public ActionResult Edit(int id)
+    {
+        var propietario = repository.ObtenerPorId(id);
+        if (propietario == null)
+        {
+            return NotFound(); 
+        }
+        return View(propietario); 
+    }
+
+    // POST: Propietarios/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(Propietario propietario)
+    {
+        if (ModelState.IsValid)
+        {
+            repository.Modificar(propietario);
+            TempData["Mensaje"] = "Propietario actualizado correctamente";
+            return RedirectToAction(nameof(Index));
+        }
+        return View(propietario);
+    }
+
+
+
+}
